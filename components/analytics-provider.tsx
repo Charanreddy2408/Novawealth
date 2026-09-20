dddb0d69dc765e7aeb3d96e096f699276179dfea
Charanreddy2408 <akepatisricharan@gmail.com>
2026-09-20 13:39:28 +0530

    Update phone number and add Google Analytics deployment architecture

--- components/analytics-provider.tsx
+++ components/analytics-provider.tsx
@@ -1,27 +1,47 @@
 import Script from "next/script";
 
+import { siteConfig } from "@/lib/site-config";
+
 export function AnalyticsProvider() {
-  const id = process.env.NEXT_PUBLIC_GTM_ID;
-  if (!id) return null;
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const gaId = siteConfig.googleAnalyticsId;
 
   return (
     <>
-      <Script id="gtm-loader" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${id}');`}
-      </Script>
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${id}`}
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-          title="Google Tag Manager"
-        />
-      </noscript>
+      {gtmId && (
+        <>
+          <Script id="gtm-loader" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${gtmId}');`}
+          </Script>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+              title="Google Tag Manager"
+            />
+          </noscript>
+        </>
+      )}
+
+      {gaId && (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}');
+            `}
+          </Script>
+        </>
+      )}
     </>
   );
 }
--- lib/site-config.ts
+++ lib/site-config.ts
@@ -8,6 +8,7 @@ export const siteConfig = {
   email: process.env.NEXT_PUBLIC_EMAIL ?? "hello@novawealth.com.au",
   location: "Australia-wide advice",
   calendlyUrl: process.env.NEXT_PUBLIC_CALENDLY_URL ?? "",
+  googleAnalyticsId: process.env.NEXT_PUBLIC_GA_ID ?? "",
   scoreAppUrl: process.env.NEXT_PUBLIC_SCOREAPP_URL ?? "",
   linkedInUrl: process.env.NEXT_PUBLIC_LINKEDIN_URL ?? "#",
   tikTokUrl: process.env.NEXT_PUBLIC_TIKTOK_URL ?? "#",
